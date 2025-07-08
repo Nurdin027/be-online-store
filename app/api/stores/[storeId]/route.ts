1f9ef39df6ff267cdaab39720e6c9aa54d463ae0
Nurdin027 <amarnurdin000@gmil.com>
2025-07-08 20:20:37 +0700

    metadata params apis

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -1,31 +1,32 @@
 import db from "@/lib/db";
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import {auth} from "@clerk/nextjs";
+import {NextResponse} from "next/server";
 
 export async function PATCH(
   req: Request,
-  { params }: { params: { storeId: string } }
+  {params}: { params: Promise<{ storeId: string }> }
 ) {
   try {
-    const { userId } = auth();
+    const {storeId} = await params
+    const {userId} = auth();
     const body = await req.json();
 
-    const { name } = body;
+    const {name} = body;
 
     if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
+      return new NextResponse("Unauthenticated", {status: 401});
     }
     if (!name) {
-      return new NextResponse("Harus menginput nama", { status: 400 });
+      return new NextResponse("Harus menginput nama", {status: 400});
     }
 
-    if (!params.storeId) {
-      return new NextResponse("Store id dibutuhkan", { status: 400 });
+    if (!storeId) {
+      return new NextResponse("Store id dibutuhkan", {status: 400});
     }
 
     const store = await db.store.updateMany({
       where: {
-        id: params.storeId,
+        id: storeId,
         userId,
       },
       data: {
@@ -36,36 +37,36 @@ export async function PATCH(
     return NextResponse.json(store);
   } catch (error) {
     console.log("[STORE_PATCH]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    return new NextResponse("Internal error", {status: 500});
   }
 }
 
 export async function DELETE(
-    req: Request,
-    { params }: { params: { storeId: string } }
-  ) {
-    try {
-      const { userId } = auth();
-  
-  
-      if (!userId) {
-        return new NextResponse("Unauthenticated", { status: 401 });
-      }
-  
-      if (!params.storeId) {
-        return new NextResponse("Store id dibutuhkan", { status: 400 });
-      }
-  
-      const store = await db.store.deleteMany({
-        where: {
-          id: params.storeId,
-          userId,
-        },
-      });
-  
-      return NextResponse.json(store);
-    } catch (error) {
-      console.log("[STORE_DELETE]", error);
-      return new NextResponse("Internal error", { status: 500 });
+  req: Request,
+  {params}: { params: Promise<{ storeId: string }> }
+) {
+  try {
+    const {storeId} = await params
+    const {userId} = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", {status: 401});
     }
+
+    if (!storeId) {
+      return new NextResponse("Store id dibutuhkan", {status: 400});
+    }
+
+    const store = await db.store.deleteMany({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_DELETE]", error);
+    return new NextResponse("Internal error", {status: 500});
   }
+}
--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,20 +1,20 @@
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
+import {auth} from "@clerk/nextjs";
+import {NextResponse} from "next/server";
 import db from "@/lib/db";
 
 export async function POST(req: Request) {
   try {
-    const { userId } = auth();
+    const {userId} = auth();
     const body = await req.json();
 
-    const { name } = body;
+    const {name} = body;
 
     if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
+      return new NextResponse("Unauthorized", {status: 401});
     }
 
     if (!name) {
-      return new NextResponse("Nama toko perlu diinput", { status: 400 });
+      return new NextResponse("Nama toko perlu diinput", {status: 400});
     }
 
     const store = await db.store.create({
@@ -27,6 +27,6 @@ export async function POST(req: Request) {
     return NextResponse.json(store);
   } catch (error) {
     console.log("[STORES_POST]", error);
-    return new NextResponse("Internal error", { status: 500 });
+    return new NextResponse("Internal error", {status: 500});
   }
 }
